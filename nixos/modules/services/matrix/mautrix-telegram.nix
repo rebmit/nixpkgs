61019bb54962ccd833d4430976c5726844d856cf
Nick Cao <nickcao@nichi.co>
2026-04-15 21:15:53 -0400

    HACK: nixos/mautrix-telegram: adapt for the go rewrite

--- nixos/modules/services/matrix/mautrix-telegram.nix
+++ nixos/modules/services/matrix/mautrix-telegram.nix
@@ -18,82 +18,16 @@ in
     services.mautrix-telegram = {
       enable = lib.mkEnableOption "Mautrix-Telegram, a Matrix-Telegram hybrid puppeting/relaybot bridge";
 
-      package = lib.mkPackageOption pkgs "mautrix-telegram" { };
+      package = lib.mkPackageOption pkgs "mautrix-telegram-go" { };
 
       settings = lib.mkOption rec {
         apply = lib.recursiveUpdate default;
         inherit (settingsFormat) type;
-        default = {
-          homeserver = {
-            software = "standard";
-          };
-
-          appservice = rec {
-            database = "sqlite:///${dataDir}/mautrix-telegram.db";
-            database_opts = { };
-            hostname = "0.0.0.0";
-            port = 8080;
-            address = "http://localhost:${toString port}";
-          };
-
-          bridge = {
-            permissions."*" = "relaybot";
-            relaybot.whitelist = [ ];
-            double_puppet_server_map = { };
-            login_shared_secret_map = { };
-          };
-
-          logging = {
-            version = 1;
-
-            formatters.precise.format = "[%(levelname)s@%(name)s] %(message)s";
-
-            handlers.console = {
-              class = "logging.StreamHandler";
-              formatter = "precise";
-            };
-
-            loggers = {
-              mau.level = "INFO";
-              telethon.level = "INFO";
-
-              # prevent tokens from leaking in the logs:
-              # https://github.com/tulir/mautrix-telegram/issues/351
-              aiohttp.level = "WARNING";
-            };
-
-            # log to console/systemd instead of file
-            root = {
-              level = "INFO";
-              handlers = [ "console" ];
-            };
-          };
-        };
-        example = lib.literalExpression ''
-          {
-            homeserver = {
-              address = "http://localhost:8008";
-              domain = "public-domain.tld";
-            };
-
-            appservice.public = {
-              prefix = "/public";
-              external = "https://public-appservice-address/public";
-            };
-
-            bridge.permissions = {
-              "example.com" = "full";
-              "@admin:example.com" = "admin";
-            };
-            telegram = {
-              connection.use_ipv6 = true;
-            };
-          }
-        '';
+        default = { };
         description = ''
           {file}`config.yaml` configuration as a Nix attribute set.
           Configuration options should match those described in
-          [example-config.yaml](https://github.com/mautrix/telegram/blob/master/mautrix_telegram/example-config.yaml).
+          [example-config.yaml](https://docs.mau.fi/configs/mautrix-telegram/latest).
 
           Secret tokens should be specified using {option}`environmentFile`
           instead of this world-readable attribute set.
@@ -105,23 +39,7 @@ in
         default = null;
         description = ''
           File containing environment variables to be passed to the mautrix-telegram service,
-          in which secret tokens can be specified securely by defining values for e.g.
-          `MAUTRIX_TELEGRAM_APPSERVICE_AS_TOKEN`,
-          `MAUTRIX_TELEGRAM_APPSERVICE_HS_TOKEN`,
-          `MAUTRIX_TELEGRAM_TELEGRAM_API_ID`,
-          `MAUTRIX_TELEGRAM_TELEGRAM_API_HASH` and optionally
-          `MAUTRIX_TELEGRAM_TELEGRAM_BOT_TOKEN`.
-
-          These environment variables can also be used to set other options by
-          replacing hierarchy levels by `.`, converting the name to uppercase
-          and prepending `MAUTRIX_TELEGRAM_`.
-          For example, the first value above maps to
-          {option}`settings.appservice.as_token`.
-
-          The environment variable values can be prefixed with `json::` to have
-          them be parsed as JSON. For example, `login_shared_secret_map` can be
-          set as follows:
-          `MAUTRIX_TELEGRAM_BRIDGE_LOGIN_SHARED_SECRET_MAP=json::{"example.com":"secret"}`.
+          in which secret tokens can be specified securely.
         '';
       };
 
@@ -190,14 +108,10 @@ in
       environment.HOME = dataDir;
 
       preStart = ''
-        # substitute the settings file by environment variables
-        # in this case read from EnvironmentFile
         test -f '${settingsFile}' && rm -f '${settingsFile}'
         old_umask=$(umask)
         umask 0177
-        ${pkgs.envsubst}/bin/envsubst \
-          -o '${settingsFile}' \
-          -i '${settingsFileUnsubstituted}'
+        cp '${settingsFileUnsubstituted}' '${settingsFile}'
         umask $old_umask
 
         # generate the appservice's registration file if absent
@@ -220,10 +134,6 @@ in
         mv '${settingsFile}.tmp' '${settingsFile}'
 
         umask $old_umask
-      ''
-      + lib.optionalString (cfg.package ? alembic) ''
-        # run automatic database init and migration scripts
-        ${cfg.package.alembic}/bin/alembic -x config='${settingsFile}' upgrade head
       '';
 
       serviceConfig = {
@@ -239,7 +149,6 @@ in
         ProtectControlGroups = true;
 
         PrivateTmp = true;
-        WorkingDirectory = cfg.package; # necessary for the database migration scripts to be found
         StateDirectory = baseNameOf dataDir;
         UMask = "0027";
         EnvironmentFile = cfg.environmentFile;
